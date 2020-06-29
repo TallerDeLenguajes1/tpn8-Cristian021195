a69716e91a24535144f279b81f240da0fee2859b
Cristian021195 <cristiangramajo015@gmail.com>
2020-06-28 21:09:31 -0300

    Se agrego archivo carpeta con las clases usadas

---
+++ Clases/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace tpn7.Clases
+{
+    public class Calculadora
+    {
+        private float x;
+        private float y;
+        private string z;
+        private DateTime fecha;
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public string Z { get => z; set => z = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+
+        public string resultado(float X, float Y, string Z, DateTime Fecha) {
+            string res="";
+            if (Z == "+") {
+                res = (X + Y).ToString();
+            }else if(Z == "-")
+            {
+                res = (X - Y).ToString();
+            }
+            else if(Z == "*")
+            {
+                res = (X * Y).ToString();
+            }
+            else if(Z == "/")
+            {
+                if (Y != 0) {
+                    res = (X / Y).ToString();
+                }
+                else { res = "ERROR"; }
+            }
+
+            //return X.ToString() + "#" + Z + "#" + Y.ToString() + "#" + res + "#" + Fecha.ToString();
+            return res;
+        }
+    }
+}
